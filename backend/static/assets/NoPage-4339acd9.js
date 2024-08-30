f69d5280a288a44c1c8e3f38817924e7063215bf
Indraja <A7321@axtria.com>
2024-08-30 13:36:10 +0530

    removed backend

--- backend/static/assets/NoPage-4339acd9.js
+++
@@ -1,2 +0,0 @@
-import{a$ as o}from"./vendor-e8e62dad.js";function e(){return o.jsx("h1",{children:"404"})}e.displayName="NoPage";export{e as Component};
-//# sourceMappingURL=NoPage-4339acd9.js.map
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/teams_CHAT/",
+    base: "/sid/",
     build: {
         outDir: "../backend/static",
         emptyOutDir: true,

3a0d8399c56ef325ea05ecc9f0e6d701b917cf5e
Indraja <A7321@axtria.com>
2024-08-30 17:51:12 +0530

    Updates

--- static/assets/NoPage-726b6c8a.js
+++
@@ -1,2 +0,0 @@
-import{a$ as o}from"./vendor-78db3ebb.js";function e(){return o.jsx("h1",{children:"404"})}e.displayName="NoPage";export{e as Component};
-//# sourceMappingURL=NoPage-726b6c8a.js.map

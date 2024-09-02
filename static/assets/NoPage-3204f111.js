b98e5bf556f9073c2f7b931dd047429a6816fa76
Indraja <A7321@axtria.com>
2024-09-02 17:00:36 +0530

    Updates

--- static/assets/NoPage-3204f111.js
+++
@@ -1,2 +0,0 @@
-import{a$ as o}from"./vendor-324be085.js";function e(){return o.jsx("h1",{children:"404"})}e.displayName="NoPage";export{e as Component};
-//# sourceMappingURL=NoPage-3204f111.js.map
